8cd0d9e5d8f01997d85dfb0d7a3a21a2932af797
Thanhluan2010 <luanlay2010@gmail.com>
2026-07-21 18:37:01 +0700

    featrue: change port

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Vua Xổ Số API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Server running on port ${PORT} (0.0.0.0)`);
